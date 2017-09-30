f7cd2cd22dcf6ee362149f72510f8c2a26d682ce
Mike Vessia <michaelvessia@mail.adelphi.edu>
2017-09-30 00:17:22 -0400

    Add basic style with mui

--- js/app.js
+++ js/app.js
@@ -2,18 +2,20 @@ var liftSection = new Vue({
   el: '#liftsTable',
   data: {
     lifts: [
-      { name: 'squat', reps: 0, weight: 0, oneRepMax: 0}, 
-      { name: 'bench', reps: 0, weight: 0, oneRepMax: 0},
-      { name: 'deadlift', reps: 0, weight: 0, oneRepMax: 0},
-      { name: 'press', reps: 0, weight: 0, oneRepMax: 0}
+      { name: 'squat', reps: 0, weight: 0, oneRepMax: 0, trainingMax: 0}, 
+      { name: 'bench', reps: 0, weight: 0, oneRepMax: 0, trainingMax: 0},
+      { name: 'deadlift', reps: 0, weight: 0, oneRepMax: 0, trainingMax: 0},
+      { name: 'press', reps: 0, weight: 0, oneRepMax: 0, trainingMax: 0}
     ]
   },
   methods: {
     calcOneRepMax: function (lift, reps, weight) {
       reps = parseInt(reps);
       weight = parseInt(weight);
+      // 1RM Formula taken from Wendler's 5/3/1
       lift.oneRepMax = Math.round(weight * reps * 0.0333 + weight);
-      
+      // Wendler defines training maximum is 90% of 1RM
+      lift.trainingMax = .90 * lift.oneRepMax;
       return lift.oneRepMax;
     }
   },
